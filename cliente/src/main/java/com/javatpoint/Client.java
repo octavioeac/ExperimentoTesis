cbf552170cbffd25daca2de0263896a50448c554
Octavio <Octavio@Octavio-VAIO>
2015-12-16 02:33:06 -0600

    :f

---
+++ cliente/src/main/java/com/javatpoint/Client.java
@@ -0,0 +1,13 @@
+package com.javatpoint;  
+import org.springframework.context.ApplicationContext;  
+import org.springframework.context.support.ClassPathXmlApplicationContext;  
+  
+public class Client {  
+public static void main(String[] args)  {  
+ApplicationContext context = new ClassPathXmlApplicationContext("client-beans.xml");  
+Calculation calculation = (Calculation)context.getBean("calculationBean");  
+System.out.println(calculation.cube(7));  
+
+
+}  
+}  
